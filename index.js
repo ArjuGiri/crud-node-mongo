1913358619285c1bd6db41ba3483ada9ff8a6032
Arjun <arjungiriag03@gmail.com>
2024-02-28 13:17:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// routes
+app.use("/api/products", productRoute); //doubt
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://arjungiriag03:qYWOV2kiOBlpRGH5@backenddb.2ixvku9.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
+    console.log("connection error");
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
